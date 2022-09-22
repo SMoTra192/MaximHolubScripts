4e8a2f07f06f71bdda9be47e598aa3205c742779
SMoTra192 <m.golub2002@gmail.com>
2022-09-22 18:51:41 +0300

    Venicle

---
+++ Venicle.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public abstract class Venicle
+    {
+        public abstract string autovoice();
+
+        public class car : Venicle
+        {
+            public override string autovoice() => "Врум-Врум";
+        }
+
+        public class ship : Venicle
+        {
+            public override string autovoice() => "Плыв-Плыв";
+        }
+
+        public class plane : Venicle
+        {
+            public override string autovoice() => "Виу-Виу";
+        }
+
+
+
+
+        public static class programm
+        {
+            public static void Main(string[] args)
+            {
+                var Machina = new Venicle.car();
+                var Korabl = new Venicle.ship();
+                var Samolet = new Venicle.plane();
+                Venicle[] venicles = new Venicle[] { Machina, Korabl, Samolet };
+                foreach (var Venicle_ in venicles)
+                {
+                    Console.WriteLine(Venicle_.autovoice());
+                }
+            }
+        }
+
+
+    }
+}
